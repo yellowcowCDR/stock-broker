02e64cd7e70b6791ab155e371aeea5b2f63eb85b
yellowcowCDR <eofus4250@naver.com>
2026-07-13 04:11:44 +0900

    docs: add openapi definition for swagger ui

--- src/main/java/com/hermes/broker/HermesBrokerApplication.java
+++ src/main/java/com/hermes/broker/HermesBrokerApplication.java
@@ -1,10 +1,19 @@
 package com.hermes.broker;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Hermes Trading Broker API",
+        description = "자동 매매 에이전트를 위한 주식 주문 및 시세 중계 서버 API 문서입니다.",
+        version = "v1.0"
+    )
+)
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
